34009b4387b2549079abf1310d70d819a61feedf
Ubuntu <ubuntu@ip-172-31-25-228.eu-west-2.compute.internal>
2020-12-04 08:29:39 +0000

    new bash script

--- src/generate_results.sh
+++ src/generate_results.sh
@@ -5,20 +5,20 @@
 #   rm sample_results_output.log
 # fi
 # touch sample_results_output.log
-for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 
 do
-    if [ -d ../sample_results/sample_softplus_"$i" ]
+    if [ -d ../sample_results/sample_"$i" ]
     then
-      rm -r ../sample_results/sample_softplus_"$i"
+      rm -r ../sample_results/sample_"$i"
     fi
-    mkdir ../sample_results/sample_softplus_"$i"
-    if [ -f ../sample_results/sample_softplus_"$i"/output.log ]
+    mkdir ../sample_results/sample_"$i"
+    if [ -f ../sample_results/sample_"$i"/output.log ]
     then
-      rm ../sample_results/sample_softplus_"$i"/output.log
+      rm ../sample_results/sample_"$i"/output.log
     fi
-    touch ../sample_results/sample_softplus_"$i"/output.log
-    python run_attack_softplus.py --cuda --img ../../Spatial_transform/ST_ADV_exp_imagenet/sample_imagenet/sample_"$i".jpg \
+    touch ../sample_results/sample_"$i"/output.log
+    python run_attack.py --cuda --img ../../Spatial_transform/ST_ADV_exp_imagenet/sample_imagenet/sample_"$i".jpg \
     --target_img ../../Spatial_transform/ST_ADV_exp_imagenet/sample_imagenet/sample_"$i"_target.jpg \
-    --output_dir ../sample_results/sample_softplus_"$i"/ --lr 0.0003| tee -a "sample_results_output.log"
-    echo "\n" | tee -a ../sample_results/sample_softplus_"$i"/output.log
+    --output_dir ../sample_results/sample_"$i"/ | tee -a ../sample_results/sample_"$i"/output.log
+    echo "\n" | tee -a ../sample_results/sample_"$i"/output.log
 done
